4a929cc24c341bfcef4c914c7b6587aaf278d67c
Andrew James <ajames20@gmail.com>
2019-03-24 09:05:32 -0500

    Init commit

---
+++ src/client/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+const AppContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+  font-size: 40px;
+`;
+
+const App = () => <AppContainer>Hello World</AppContainer>;
+
+export default App;
