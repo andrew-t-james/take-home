1a5d28424ba8527b3b3800ab3c9915459700317f
Andrew James <ajames20@gmail.com>
2019-03-24 09:16:36 -0500

    Add Apollo and client

--- src/server.js
+++ src/server.js
@@ -1,17 +1,38 @@
 import express from "express";
 import React from "react";
+import fetch from "isomorphic-fetch";
 import { renderToString } from "react-dom/server";
 import App from "./client/App";
 import Html from "./client/Html";
 import { ServerStyleSheet } from "styled-components";
+import { ApolloProvider } from "react-apollo";
+import { ApolloClient } from "apollo-client";
+import { createHttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
 
 const port = 3000;
 const server = express();
-
 server.get("/", (req, res) => {
   const sheet = new ServerStyleSheet();
+  const client = new ApolloClient({
+    ssrMode: true,
+    // Remember that this is the interface the SSR server will use to connect to the
+    // API server, so we need to ensure it isn't firewalled, etc
+    link: createHttpLink({
+      fetch,
+      uri: "http://api.crystallize.com/graphql"
+    }),
+    cache: new InMemoryCache()
+  });
 
-  const body = renderToString(sheet.collectStyles(<App />));
+  const body = renderToString(
+    sheet.collectStyles(
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
+    )
+  );
+
   const styles = sheet.getStyleTags();
   const title = "SSR GraphQL React with Styled Components";
 
