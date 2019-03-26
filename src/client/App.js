97d96c77c3c747ee9394ab4cae8b223ecf163018
Andrew James <ajames20@gmail.com>
2019-03-25 20:32:20 -0500

    Add query to init page

--- src/client/App.js
+++ src/client/App.js
@@ -1,15 +1,63 @@
 import React from "react";
-import styled from "styled-components";
+import gql from "graphql-tag";
+import { Query } from "react-apollo";
+import styled, { createGlobalStyle } from "styled-components";
+
+const CRYSTALLIZE_QUERY = gql`
+  {
+    catalogue(url: "/illustrations", tenantID: "demo") {
+      name
+      content_fields
+      children {
+        link
+        name
+        product {
+          name
+          product_image
+          variations {
+            variation_sku
+            stock_count
+            price_ex_vat
+          }
+        }
+      }
+    }
+  }
+`;
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+  }
+  body {
+    color: red;
+    font-size: 62.5%;
+  }
+`;
 
 const AppContainer = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
   height: 100%;
-  font-size: 40px;
-  color: red;
+  font-size: 1rem;
 `;
 
-const App = () => <AppContainer>Hello World</AppContainer>;
+const App = () => (
+  <>
+    <GlobalStyle />
+    <AppContainer>
+      <Query query={CRYSTALLIZE_QUERY}>
+        {({ loading, error, data }) => {
+          if (loading) return "Loading...";
+          if (error) return `Error! ${error.message}`;
+
+          return <>{JSON.stringify(data, null, 2)}</>;
+        }}
+      </Query>
+    </AppContainer>
+  </>
+);
 
 export default App;
