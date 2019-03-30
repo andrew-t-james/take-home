7082a6d5aaad9526cd659530ba73e9ef7d04ec7e
Andrew James <ajames20@gmail.com>
2019-03-29 19:42:48 -0500

    Add grid and some simple styles

--- src/public/client/App.js
+++ src/public/client/App.js
@@ -28,28 +28,55 @@ const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
   }
+
   body {
-    color: red;
+    color: rgb(244, 127, 152);
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
     font-size: 62.5%;
   }
 `;
 
 const AppContainer = styled.div`
+  max-width: 1024px;
+  margin: 0 auto;
+`;
+
+const StyledList = styled.div`
   display: flex;
+  display: grid;
+  grid-column-gap: 1rem;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  padding: 2rem;
+
+  @media screen and (max-width: 436px) {
+    grid-template-columns: repeat(auto-fit, minmax(400px, 1fr));
+  }
+
+  @media screen and (max-width: 350px) {
+    grid-template-columns: repeat(auto-fit, minmax(260px, 1fr));
+  }
+`;
+
+const StyledListItem = styled.div`
+  border-radius: 0.4rem;
+  text-align: center;
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
   align-items: center;
-  justify-content: center;
-  height: 100%;
-  font-size: 1rem;
-`;
+  box-shadow: rgba(0, 0, 0, 0.05) 3px 14px 24px;
+  background: rgb(255, 255, 255);
+  border: 1px solid rgb(223, 223, 223);
+  margin: 2rem;
+  padding: 2rem;
 
-const StyledList = styled.ul`
-  display: flex;
-  list-style: none;
-`;
+  h2 {
+    font-size: 1.4rem;
+    margin-bottom: 1.5rem;
+  }
 
-const StyledListItem = styled.li`
   img {
-    height: 6rem;
+    height: 10rem;
   }
 `;
 
